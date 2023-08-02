18c19d469e1b15b737cc765ad7f068cc5b73ec5c
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-02 19:20:55 +0545

    multi-step-form using React

---
+++ src/components/UserForm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const UserForm = () => {
+    state= {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email:'',
+        occupation: '',
+        city: '',
+        bio: ''
+    }
+
+    //Procced to next step
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserForm
