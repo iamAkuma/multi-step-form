427f1dfd840d78518fe181fbe564c8ff81126700
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-05 19:09:54 +0545

    UI changes

--- src/components/FormPersonalDetails.js
+++ src/components/FormPersonalDetails.js
@@ -1,88 +1,92 @@
-import * as React from 'react';
-import { Dialog, AppBar, ThemeProvider, createTheme } from '@mui/material';
+import React from 'react';
+import styled from 'styled-components';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import styled from 'styled-components';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 const theme = createTheme();
 
-const StyledModal = styled(Dialog)`
-  .MuiDialog-paper {
-    display: flex;
-    flex-direction: column;
-    padding: 20px;
-    border-radius: 10px;
-  }
+const StyledModal = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
 
-const FormContainer = styled.div`
+const StyledCard = styled(Card)`
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  align-items: center;
+  width: 400px;
+  padding: 20px;
+`;
+
+const Title = styled(Typography)`
+  margin-bottom: 20px;
+`;
+
+const StyledTextField = styled(TextField)`
+  margin-bottom: 15px;
+`;
+
+const StyledButton = styled(Button)`
   margin-top: 20px;
 `;
 
-const ButtonsContainer = styled.div`
-  display: flex;
-  gap: 15px;
-  justify-content: flex-end;
-  margin-top: 20px;
-`;
-
-export default function FormPersonalDetails(props) {
-  const { values, handleChange } = props;
-
+const FormUserDetails = ({ values, handleChange, nextStep }) => {
   const continueStep = (e) => {
     e.preventDefault();
-    props.nextStep();
+    nextStep();
   };
 
-  const back = (e) => {
-    e.preventDefault();
-    props.prevStep();
+  const prevStep = (e) => {
+    
+    prevStep();
   };
+
+  
 
   return (
     <ThemeProvider theme={theme}>
-      <>
-        <StyledModal open fullWidth maxWidth="sm">
-          <AppBar title="Enter Personal Details" />
-          <FormContainer>
-            <TextField
+      <StyledModal>
+        <StyledCard>
+          <CardContent>
+            <Title variant="h5">Enter Personal Details</Title>
+            <StyledTextField
               placeholder="Enter Your Occupation"
               label="Occupation"
               onChange={handleChange('occupation')}
               defaultValue={values.occupation}
-              margin="normal"
               fullWidth
             />
-            <TextField
+            <StyledTextField
               placeholder="Enter Your City"
               label="City"
               onChange={handleChange('city')}
               defaultValue={values.city}
-              margin="normal"
               fullWidth
             />
-            <TextField
+            <StyledTextField
               placeholder="Enter Your Bio"
               label="Bio"
               onChange={handleChange('bio')}
               defaultValue={values.bio}
-              margin="normal"
               fullWidth
             />
-          </FormContainer>
-          <ButtonsContainer>
-            <Button color="secondary" variant="contained" onClick={back}>
+             <StyledButton color="secondary" variant="contained" onClick={prevStep}>
               Back
-            </Button>
-            <Button color="primary" variant="contained" onClick={continueStep}>
+            </StyledButton>
+            <StyledButton color="primary" variant="contained" onClick={continueStep}>
               Continue
-            </Button>
-          </ButtonsContainer>
-        </StyledModal>
-      </>
+            </StyledButton>
+          </CardContent>
+        </StyledCard>
+      </StyledModal>
     </ThemeProvider>
   );
-}
+};
+
+export default FormUserDetails;
--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -3,19 +3,29 @@ import styled from 'styled-components';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 const theme = createTheme();
 
 const StyledModal = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  height: 100vh;
 `;
 
-const Title = styled.h2`
+const StyledCard = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+  padding: 20px;
+`;
+
+const Title = styled(Typography)`
   margin-bottom: 20px;
-  color: #333;
 `;
 
 const StyledTextField = styled(TextField)`
@@ -35,31 +45,35 @@ const FormUserDetails = ({ values, handleChange, nextStep }) => {
   return (
     <ThemeProvider theme={theme}>
       <StyledModal>
-        <Title>Enter User Details</Title>
-        <StyledTextField
-          placeholder="Enter Your First Name"
-          label="First Name"
-          onChange={handleChange('firstName')}
-          defaultValue={values.firstName}
-          fullWidth
-        />
-        <StyledTextField
-          placeholder="Enter Your Last Name"
-          label="Last Name"
-          onChange={handleChange('lastName')}
-          defaultValue={values.lastName}
-          fullWidth
-        />
-        <StyledTextField
-          placeholder="Enter Your Email"
-          label="Email"
-          onChange={handleChange('email')}
-          defaultValue={values.email}
-          fullWidth
-        />
-        <StyledButton color="primary" variant="contained" onClick={continueStep}>
-          Continue
-        </StyledButton>
+        <StyledCard>
+          <CardContent>
+            <Title variant="h5">Enter User Details</Title>
+            <StyledTextField
+              placeholder="Enter Your First Name"
+              label="First Name"
+              onChange={handleChange('firstName')}
+              defaultValue={values.firstName}
+              fullWidth
+            />
+            <StyledTextField
+              placeholder="Enter Your Last Name"
+              label="Last Name"
+              onChange={handleChange('lastName')}
+              defaultValue={values.lastName}
+              fullWidth
+            />
+            <StyledTextField
+              placeholder="Enter Your Email"
+              label="Email"
+              onChange={handleChange('email')}
+              defaultValue={values.email}
+              fullWidth
+            />
+            <StyledButton color="primary" variant="contained" onClick={continueStep}>
+              Continue
+            </StyledButton>
+          </CardContent>
+        </StyledCard>
       </StyledModal>
     </ThemeProvider>
   );
